1b1b8617371f1695f1d22776834c1760db684039
AyBalatsan <AyBalatsan@yandex.ru>
2021-07-31 09:39:17 +0600

    version 02

--- source/js/main.js
+++ source/js/main.js
@@ -1,9 +1,11 @@
-// import './authorization.js';
-// import './inform-block.js';
 import './navigation.js';
 import './simplebar.js';
 import './scroll.js';
+import './slider.js';
+
+// import './authorization.js';
+// import './inform-block.js';
+
 // import './other-information.js';
-// import './slayder.js';
 
 
---
+++ source/js/slider.js
@@ -0,0 +1,47 @@
+const slides = document.querySelectorAll(".slider__item");
+const dots = document.querySelectorAll(".toggle__item");
+const slideIndex = 1;
+
+function showSlides(slideIndex) {
+  let i;
+  for (i = 0; i < slides.length; i++) {
+      slides[i].classList.add("visually-hidden");
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace("toggle__item--active", "");
+  }
+  if (window.matchMedia("(min-width: 768px)").matches) {
+    slides[slideIndex - 1].classList.remove("visually-hidden");
+    dots[slideIndex - 1].className += " toggle__item--active";
+  }
+}
+
+dots.forEach((item) =>
+  item.addEventListener('click', () => {
+    showSlides(item.id);
+  })
+);
+
+showSlides(slideIndex);
+window.addEventListener('resize', () => {
+  if (window.matchMedia("(min-width: 768px)").matches) {
+    let i;
+    for (i = 0; i < slides.length; i++) {
+      slides[i].classList.add("visually-hidden");
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace("toggle__item--active", "");
+    }
+    slides[slideIndex - 1].classList.remove("visually-hidden");
+    dots[slideIndex - 1].className += " toggle__item--active";
+  }
+  else{
+    let i;
+    for (i = 0; i < slides.length; i++) {
+      slides[i].classList.add("visually-hidden");
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace("toggle__item--active", "");
+    }
+  }
+});
