7f3e9cf3205e1f939064476499ad7c9b70d1462d
AyBalatsan <AyBalatsan@yandex.ru>
2021-08-01 15:35:08 +0600

    v3

--- source/js/slider.js
+++ source/js/slider.js
@@ -5,13 +5,13 @@ const slideIndex = 1;
 function showSlides(slideIndex) {
   let i;
   for (i = 0; i < slides.length; i++) {
-      slides[i].classList.add("visually-hidden");
+    slides[i].classList.remove("slider__item--current");
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace("toggle__item--active", "");
   }
   if (window.matchMedia("(min-width: 768px)").matches) {
-    slides[slideIndex - 1].classList.remove("visually-hidden");
+    slides[slideIndex - 1].classList.add("slider__item--current");
     dots[slideIndex - 1].className += " toggle__item--active";
   }
 }
@@ -27,18 +27,18 @@ window.addEventListener('resize', () => {
   if (window.matchMedia("(min-width: 768px)").matches) {
     let i;
     for (i = 0; i < slides.length; i++) {
-      slides[i].classList.add("visually-hidden");
+      slides[i].classList.remove("slider__item--current");
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace("toggle__item--active", "");
     }
-    slides[slideIndex - 1].classList.remove("visually-hidden");
+    slides[slideIndex - 1].classList.add("slider__item--current");
     dots[slideIndex - 1].className += " toggle__item--active";
   }
   else{
     let i;
     for (i = 0; i < slides.length; i++) {
-      slides[i].classList.add("visually-hidden");
+      slides[i].classList.remove("slider__item--current");
     }
     for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace("toggle__item--active", "");
